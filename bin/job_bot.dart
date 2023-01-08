33ef46d4a596e59ac7530fc5827f0f6100fe9b53
PepperoniOnzo <radchukmaxym@gmail.com>
2023-01-08 14:18:13 +0200

    Init

---
+++ bin/job_bot.dart
@@ -0,0 +1,5 @@
+import 'package:job_bot/job_bot.dart' as job_bot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${job_bot.calculate()}!');
+}
---
+++ test/job_bot_test.dart
@@ -0,0 +1,6 @@
+import 'package:job_bot/job_bot.dart';
+import 'package:test/test.dart';
+
+void main() {
+
+}
