f4150db29cf3955e378d187ee731520b366d2315
PepperoniOnzo <radchukmaxym@gmail.com>
2023-01-08 16:54:20 +0200

    Comfigurations

---
+++ lib/constants/configuration.dart
@@ -0,0 +1,20 @@
+class Configuration {
+  /// Telegram bot token
+  static const String token = '';
+
+  /// Save data path
+  static const String saveDataPath = 'db/data.json';
+
+  // Time of days
+  /// Morning time of day
+  static const int morning = 12;
+
+  /// Afternoon time of day
+  static const int afternoon = 15;
+
+  /// Evening time of day
+  static const int evening = 18;
+
+  /// Night time of day
+  static const int night = 21;
+}
